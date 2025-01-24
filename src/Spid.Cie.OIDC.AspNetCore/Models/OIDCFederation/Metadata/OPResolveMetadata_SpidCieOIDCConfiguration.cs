c48d9a255d6a49ecce2b0e7518fdea398147177c
tempcode <piemp@hotmail.it>
2025-01-24 15:29:41 +0100

    fix: added specific object for resolve endpoint
    fix: added object to allow serialization of openid_provider

---
+++ src/Spid.Cie.OIDC.AspNetCore/Models/OIDCFederation/Metadata/OPResolveMetadata_SpidCieOIDCConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json.Serialization;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+
+namespace Spid.Cie.OIDC.AspNetCore.Models;
+
+[ExcludeFromCodeCoverage]
+sealed class OPResolveMetadata_SpidCieOIDCConfiguration
+{
+    [JsonPropertyName("openid_provider")]
+    public OpenIdConnectConfiguration? OpenIdProvider { get; set; }
+}
