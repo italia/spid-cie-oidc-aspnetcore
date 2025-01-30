daf15cd1200017d43048a71a8702cdd76242d6ba
tempcode <piemp@hotmail.it>
2025-01-30 10:32:42 +0100

    style: removed unuseful using

--- src/Spid.Cie.OIDC.AspNetCore/Middlewares/FetchOpenIdFederationMiddleware.cs
+++ src/Spid.Cie.OIDC.AspNetCore/Middlewares/FetchOpenIdFederationMiddleware.cs
@@ -8,7 +8,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
 using System.Threading.Tasks;
