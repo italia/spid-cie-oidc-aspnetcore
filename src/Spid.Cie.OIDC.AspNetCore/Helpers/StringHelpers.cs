5a8c233a066fe8f16d85fc4cbbfd3a597bb5b77c
tempcode <piemp@hotmail.it>
2025-01-30 10:15:14 +0100

    feat: added trust mark gen in fetch endpoint
    feat: trust mark gen if empty
    feat: trust mark gen if expired
    feat: added helper to get index of nth occur
    feat: added code to ignore item in TrustMarkPayload

--- src/Spid.Cie.OIDC.AspNetCore/Helpers/StringHelpers.cs
+++ src/Spid.Cie.OIDC.AspNetCore/Helpers/StringHelpers.cs
@@ -25,4 +25,13 @@ static class StringHelpers
 
         return url;
     }
+
+    public static int IndexOfOccurence(this string s, string match, int occurence)
+    {
+        var idx = s.IndexOf(match, 0);
+        var nth = occurence;
+        while (idx >= 0 && --nth > 0)
+            idx = s.IndexOf(match, idx + 1);
+        return idx;
+    }
 }
--- src/Spid.Cie.OIDC.AspNetCore/Middlewares/FetchOpenIdFederationMiddleware.cs
+++ src/Spid.Cie.OIDC.AspNetCore/Middlewares/FetchOpenIdFederationMiddleware.cs
@@ -8,6 +8,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Runtime.CompilerServices;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
 using System.Threading.Tasks;
@@ -73,7 +74,7 @@ class FetchOpenIdFederationMiddleware
             return;
         }
 
-        var certificate = aggregate?.OpenIdFederationCertificates?.FirstOrDefault();
+        var certificate = aggregate.OpenIdFederationCertificates?.FirstOrDefault();
 
         if (certificate is null)
         {
@@ -103,17 +104,34 @@ class FetchOpenIdFederationMiddleware
             return;
         }
 
-        if (relyingParty.TrustMarks is null || !relyingParty.TrustMarks.Any())
+        //check trust mark and return entity statement with new trust mark
+        if (!RPHaveValidTrustMark(cryptoService, relyingParty, aggregate, out var trustmark))
         {
-            var id = $"{uri.Scheme}://{uri.Host}/openid_relying_party/{relyingParty.OrganizationType?.ToLower()}";
+            var aggrTrustMarkId = aggregate.TrustMarks.FirstOrDefault()?.Id ?? "";
+            //if I don't have Id in aggr trust mark or Id doesn't contain a valid url 
+            if ( string.IsNullOrEmpty(aggrTrustMarkId) && aggrTrustMarkId.IndexOfOccurence("/",3) == -1)
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.Found;
+                context.Response.ContentType = SpidCieConst.JsonContentType;
+                await context.Response.WriteAsync(JsonSerializer.Serialize(new GenericError()
+                {
+                    ErrorCode = ErrorCodes.invalid_client,
+                    ErrorDescription = "Invalid TrustMarks for 'iss'"
+                }));
+                await context.Response.Body.FlushAsync();
+                return;
+            }
 
+            var id = $"{aggrTrustMarkId.Substring(0,aggrTrustMarkId.IndexOfOccurence("/",3))}/openid_relying_party/{relyingParty.OrganizationType?.ToLower()}";
+
+            var emission = DateTimeOffset.Now;
             var trustMark = new TrustMarkPayload() {
                 Subject = relyingParty.Id,
                 Issuer = aggregate.Id,
                 OrganizationType = relyingParty.OrganizationType?.ToLower(),
                 Id = id,
-                ExpiresOn = DateTimeOffset.Now.AddYears(1),
-                IssuedAt = DateTimeOffset.Now,
+                ExpiresOn = new DateTimeOffset(emission.Year, emission.Month, emission.Day, 0, 0, 0, emission.TimeOfDay).AddYears(1),
+                IssuedAt = emission,
             };
 
 
@@ -152,6 +170,7 @@ class FetchOpenIdFederationMiddleware
             return;
         }
 
+        // return entity statement with actual trust mark 
         var response = new EntityStatement()
         {
             ExpiresOn = DateTimeOffset.UtcNow.AddMinutes(SpidCieConst.EntityConfigurationExpirationInMinutes),
@@ -177,5 +196,39 @@ class FetchOpenIdFederationMiddleware
         await context.Response.WriteAsync(token);
         await context.Response.Body.FlushAsync();
         return;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="cryptoService"></param>
+    /// <param name="rp"></param>
+    /// <param name="aggregate"></param>
+    /// <param name="trustmark"></param>
+    /// <returns> true, if RP have a valid trust mark for the aggregate. False, instead.</returns>
+    private bool RPHaveValidTrustMark(ICryptoService cryptoService, RelyingParty rp, Aggregator aggregate, out TrustMarkDefinition trustmark) 
+    {
+        trustmark = new TrustMarkDefinition();
+
+        if (rp.TrustMarks is null || !rp.TrustMarks.Any())
+        {
+            return false;
+        }
+
+        trustmark = rp.TrustMarks.Where(x => x.Issuer == aggregate.Id).FirstOrDefault();
+
+        if (trustmark == default || string.IsNullOrEmpty(trustmark.TrustMark))
+        {
+            return false;
+        }
+
+        var tmp = JsonSerializer.Deserialize<TrustMarkPayload>(cryptoService.DecodeJWT(trustmark.TrustMark));
+
+        if (tmp is null || tmp is not null && tmp.ExpiresOn <= DateTimeOffset.Now)
+        {
+            return false;
+        }
+
+        return true;
     }
 }
